8e6a6cf665d44c110db674d483a1520f8ee2556f
Ashish <ashishjaitiwari15112000@gmail.com>
2020-09-22 18:57:37 +0530

    process-struct

--- process-creation-api/test.c
+++ process-creation-api/test.c
@@ -1,11 +1,4 @@
 #include<stdio.h>
-#include<unistd.h>
-#include <sys/wait.h>
-#include <fcntl.h>
 int main(){
-    FILE * f;
-    f=fopen("./test.txt","r");
-    int rc=fork();
-    if(rc==0) fprintf(f,"<==in child..\n");
-    else fprintf(f,".<==in parent.\n");
+    printf("I am 2");
 }
